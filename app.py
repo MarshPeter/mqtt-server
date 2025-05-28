81b211ee972d7cb58e381bd6848f0f2f46d0a3ac
MarshPeter <peter.m.farmer.204@gmail.com>
2025-05-28 23:56:58 +1000

    server should hopefully work

---
+++ app.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import paho.mqtt.client as mqtt
+import os
+
+# load_dotenv() # Use when doing local dev
+
+MQTT_BROKER = os.environ.get("MQTT_BROKER")
+MQTT_PORT = int(os.environ.get("MQTT_PORT"))
+MQTT_TOPIC = os.environ.get("MQTT_TOPIC")
+MQTT_USERNAME= os.environ.get("MQTT_USERNAME")
+MQTT_PASSWORD= os.environ.get("MQTT_PASSWORD")
+
+app = Flask(__name__)
+
+def on_cloud_connect(client, userdata, flags, rc):
+    print("[MQTT] Connected with result code", rc)
+
+def on_cloud_message(client, userdata, msg):
+    print("[MQTT] Received:", msg.topic, msg.payload.decode())
+
+mqtt_client = mqtt.Client()
+mqtt_client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+mqtt_client.tls_set()
+mqtt_client.on_connect = on_cloud_connect
+mqtt_client.on_message = on_cloud_message
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqtt_client.loop_start()
+
+@app.route('/broadcast', methods=['POST'])
+def broadcast_message():
+    data = request.get_json()
+    message = data.get("message", "")
+    mqtt_client.publish(MQTT_TOPIC, str({"message": message}).replace("'", '"'))
+    return jsonify({"status": "broadcasted", "message": message})
+
+@app.route('/')
+def index():
+    return "MQTT Broadcast API is running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
